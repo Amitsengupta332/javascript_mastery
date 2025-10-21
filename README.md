03bfd2e9aa846df4ff33ba7c3600b528554308d9
amitsengupta <amitsengupta332@gmail.com>
2025-10-21 22:56:12 +0600

    primitive and non primitive

--- README.md
+++ README.md
@@ -24,4 +24,12 @@ const is a function that is used to declare a variable. It is a block level vari
 const name = "John";
 ```
 
- 
+# difference between primitive data types and non-primitive data types
+
+# primitive data types:
+
+Basic building blocks in JavaScript that hold simple values like text, numbers, or true/false
+
+# non-primitive data types:
+
+A single type called Object that holds complex data, like collections or functions.
---
+++ index.js
@@ -0,0 +1,24 @@
+//  variables
+var x = 10;
+let y = 20;
+const z = 30;
+
+
+// var declare global variable
+// let declare block level variable
+// const declare constant variable
+
+
+// data types
+// primitive data types
+// non-primitive data types or referencing data types.
+
+// primitive data types
+// Basic building blocks in JavaScript that hold simple values like text, numbers, or true/false
+
+var a = 1;
+var b = "hello";
+var c = true;
+
+// non-primitive data types
+//A single type called Object that holds complex data, like collections or functions.
